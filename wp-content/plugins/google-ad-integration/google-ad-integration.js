ed61dd0bd3b21f9f28857f71bfd8106b00c98eb5
Zack Jones <zackaryj@rams.colostate.edu>
2021-12-17 13:27:07 -0700

    keep right rail ads from loading on small screens

--- wp-content/plugins/google-ad-integration/google-ad-integration.js
+++ wp-content/plugins/google-ad-integration/google-ad-integration.js
@@ -1,8 +1,13 @@
 jQuery(document).ready(function(){
     function displayGoogleAds() {
+        var isMobile = document.body.clientWidth < 752;
+        console.log(document.body.clientWidth);
         jQuery("div.gadi-ad-slot").each(function(i, e){
             var id = jQuery(e).attr("id");
-            googletag.cmd.push(function() { googletag.display(id); });
+            console.log(isMobile + " | " + id);
+            if (id.indexOf("-rr-") > -1 && isMobile) {
+                jQuery(e).parent().css({display: "none"});
+            } else googletag.cmd.push(function() { googletag.display(id); });
         });    
     }
 
