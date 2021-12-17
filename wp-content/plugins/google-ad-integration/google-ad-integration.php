228269e7b8241996bde36b5f6d1edfff50d9a853
Zack Jones <zackaryj@rams.colostate.edu>
2021-12-17 13:15:25 -0700

    Find right filter for before_main + add ad label & style

--- wp-content/plugins/google-ad-integration/google-ad-integration.php
+++ wp-content/plugins/google-ad-integration/google-ad-integration.php
@@ -6,9 +6,10 @@ Version: 1.0.0
 Author: Zack Jones
 */
 
-function zj_gadi_register_dependencies() {
+function zj_gadi_enqueue_dependencies() {
     wp_enqueue_script("gad-gpt", "https://securepubads.g.doubleclick.net/tag/js/gpt.js");
     wp_enqueue_script("google-ad-integration", plugins_url("google-ad-integration.js", __FILE__), array("gad-gpt", "jquery"), "1.0.0");
+    wp_enqueue_style("google-ad-integration", plugins_url("main.css", __FILE__), null, "1.0.0");
 }
 
 
@@ -21,7 +22,13 @@ function zj_gadi_generate_ad_slot_html($type) {
     } else if ($type == "right-rail-middle") {
         $id = "gadi-ad-slot-rr-middle";
     }
-    $ad_html = "<div class='gadi-ad-slot' id='$id'></div>";
+    $ad_html = "
+    <center>
+        <div class='gadi-ad-slot-wrapper'>
+            <div class='gadi-ad-slot-label'>Advertisment</div>
+            <div class='gadi-ad-slot' id='$id'></div>
+        </div>
+    </center>";
     return $ad_html;
 }
 
@@ -68,12 +75,17 @@ function zj_gadi_init_google_ads() {
 }
 
 function zj_gadi_init() {
-    
-    add_action( 'wp_enqueue_scripts', 'zj_gadi_register_dependencies');
+    //Enqueue js and css
+    add_action( 'wp_enqueue_scripts', 'zj_gadi_enqueue_dependencies');
+    //Add google ad code to head
     add_action( 'wp_head', 'zj_gadi_init_google_ads');
 
+    //Allot widgets to run shortcode
     add_filter( 'widget_text', 'do_shortcode' );
-    add_shortcode( 'gadi-top-ad-slot', 'zj_gadi_top_ad_slot_shortcode');
+    //Add the right rail widget shortcode
     add_shortcode( 'gadi-right-rail-ad-slot', 'zj_gadi_right_rail_ad_slot_shortcode');
+    
+    //Add ad the themes main content
+    add_filter( 'colormag_before_main', 'zj_gadi_top_ad_slot_shortcode' );
 }
 zj_gadi_init();
